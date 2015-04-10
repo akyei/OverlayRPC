d6e1eac863d6fc0ebb76b575d8e123893385a3ad
Alexander Kyei <alex.kyei94@gmail.com>
2015-04-09 23:53:43 -0400

    sometimes i like to sit around and eat poop

--- Node.rb
+++ Node.rb
@@ -2,12 +2,12 @@ require 'set'
 require 'lsp'
 require 'socket'
 
-
 class Graph
     Vertex = Struct.new(:name, :neighbors, :dist, :prev)
 
 
 class RoutingTable
+    @name 
     @unvisited = Set.new
     @visited = Set.new
     @costHash = {}
@@ -25,6 +25,13 @@ class RoutingTable
 
 
         end
+    end
+    
+    def nextHop(dest)
+        prev
+        curr = predecessor["dest"]
+        while (self.name != curr)
+            prev = curr
     end
 
 end
@@ -46,55 +53,142 @@ class Node
         # Proposed function that returns the cost if NODE has routing table entry
         # -1 returned from this function if dest isn't in the routing table.
         if (@routingTable.getCost(dest) != -1)
-            # Construct packet string from design document
-            msgPacket = "SENDMSG"
-            
+            # Construct packet string from design document -> need clarification on how to send EOF
+            msgPacket = "SENDMSG " + dest + " " + msg + "EOF"
+            # push this string to the next hop -> need network code and to find next hop from Dijkstra
+            # SendPacket placeholder/function to send a string across the network (string, nextHop)
+            # @routingTable.nextHop(dest) returns the neighbor of self needed to send message
+            SendPacket(msgPacket, @routingTable.nextHop(dest))
+            # Ideally will wait here for a response from the dest to see if resending is an issue
+        end 
     end
     
-    def ProcLSP(lsp_packet)
-        # Assume lsp "A:3 B:4 ". With this information we may discover new neighbors
-        # First, compare sequence number of lsp to this.sequence for relevance.
-        # If lsp.sequence > this.sequence, for each lsp "pair", process Dijkstra's
-        # Update neighbors from the Routing Table's keys.
-        # Fishy buisness: Make sure not to send 0 self-cost pairs.
+    def Ping(dest, numPings, delay)
+        # Initial idea here is to send a Req packet, and wait for an Ack response to see if line is live
+        # Message format from design document states "PING [DEST] [NUM] [DELAY]EOF.  I believe these
+        # are actually parameters for the function rather than being part of the message.
+        if (@routingTable.getCost(dest) != -1)
+            pingPacket = "PING " + 
+            count = 0
+            while (count != numPings)
+            # THIS ISN'T THE TIME NOR PLACE FOR THIS KIND OF INSOLENCE
+            end
+        end
+                
+    end
+    
+    def procPacket(pack_string)
+        if pack_string =~ /(\s+) /
+            if $1 == "LSP"
+                ProcLSP(pack_string)
+            end
+        end
+    end
+    
+    def LSPlowestCost(string)
+        split = string.split(" ")
+        small = Float::INFINITY
+        ret
         
-        src
-        seq
-        payload
-        payloadArr = Array.new
-        neighbors = {}
-        newNodes = Set.new
+        split.each do |value|
+            if value =~ /(\s+):(\d+)/ then
+                if self.RoutingTable["#{$1}"] != nil 
+                    if small > $2.to_i 
+                        small = $2.to_i
+                        ret = value
+                    end 
+                end 
+            end       
+            return ret  
+    end
+    
+    def ProcLSP(lsp_string)
+=begin
+      1) Begin by taking note of the source of the LSP packet - this will become the the predecesor of all of src's neighbors
+      2) Search for the lowest cost amoung the LSP packet that is also a neighbor of THIS node. i.e: If A's neighbor is B, and B is
+      the lowest cost neighbor of Src, then take note of this as a variable
+      3) Add src of LSP packet to this.routing table using the node from step two
+      4) Set the cost of src's cost equal to the cost in the LSP packet + the cost of the node from THIS to the node from step 2
+      5) Now, process all remaining nodes left in LSP packet, using src as its predecessor
+      6) Push LSP to all of THIS neighbors (sequence number will stop inifinite propogationis akis m ini
+=end
         
-        lsp_payload = lsp_packet.payload
-        if lsp_payload =~ (LSP (\s+) (\d+) (\d+) "((\s+:\d+)+)") then
+        if lsp_string =~ /LSP (\S+) (\d+) "(.*)"/ then
             match $1 with src
             match $2 with seq
             match $3 with payload
         end
         
-        if self.sequence["src"] > seq
+        
+        
+    lowestKnown = LSPlowestCost(payload)
+    lowestParse = lowestKnown.split(":")
+    # Assume function that gets the cost of a given node from a routing table
+    lowCost = lowestParse[1] + @routingTable.getCost(lowestParse[0])
+    # Assume function that updates routing table entry. If entry exists, update cost, otherwise ADD value with cost
+    @routingTable.update(src, lowCost)
+    
+    #Parsing the payload into an array of id:cost strings
+    payloadArr = payload.split(" ")
+        
+    #Parsing each element id:cost into a new neighbors hash
+    payloadArr.each do |value|
+    if value =~ /(\s+):(\d+)/ then
+        match $1 with cur
+        match $2 with curCost
+        nextCost = lowCost + curCost
+        @routingTable.update(cur, nextCost)
+    end
+    
+    @neighbors.each do |cur|
+        sendLSP(lsp_string)
+
+    
+        
+   
+        
+        
+        
+        
+        
+        
+=begin        
+        src
+        seq
+        payload
+        payloadArr = Array.new
+        neighbors = {}
+        
+        if lsp_string =~ /LSP (\s+) (\d+) (\d+) "((\s+:\d+)+)"/ then
+            match $1 with src
+            match $2 with seq
+            match $3 with payload
+        end
+        
+        # if the sequence number of the current Node is greater than the one coming in, end the function
+        if self.sequence["src"] > seq.to_i
             return
         end
         
         self.sequence["src"] = seq
         
+        #Parsing the payload into an array of id:cost strings
         payloadArr = payload.split(" ")
         
+        #Parsing each element id:cost into a new neighbors hash
         payloadArr.each do |value|
-            if value =~ ((\s+):(\d+)) then
+            if value =~ /(\s+):(\d+)/ then
                 neighbors[$1] = $2
+            end
         end
         
         
         
         
-    
+        #Creating a new node object and adding it to the current node's unvisited set
+        newNode = new Node(src, {}, neighbors, [])
+        self.routingTable.unvisited.add(newNode);
+        
     end
-    
+=end
 end
-
-
-
-
-
-
